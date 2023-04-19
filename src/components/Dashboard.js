2b44fd5ccb9c4916139558d9f7c10e9e113fec4a
J8051 <nathaniel.jessica@yahoo.ca>
2023-04-19 09:07:58 -0400

    the panel, dashboard and loading compenent

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,62 @@
 import React, { Component } from "react";
-
+import Loading from "./Loading";
 import classnames from "classnames";
 
-class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
 
-    return <main className={dashboardClasses} />;
+// Mock data
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
+
+
+
+
+class Dashboard extends Component {
+  state = {
+    loading: false,
+    focused: 1,
+  };
+
+  render() {
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+     });
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+    .map(panel => (
+     <Panel
+      key={panel.id}
+      id={panel.id}
+      label={panel.label}
+      value={panel.value}
+     />
+    ));
+
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
